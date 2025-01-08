0816dbe7e105e9f23c88516f520335b58b9ce4c4
Ashish Kumar Ojha <128972521+AshDontCode@users.noreply.github.com>
2025-01-09 02:15:30 +0530

    Update api.js

--- geospatial-frontend/src/utils/api.js
+++ geospatial-frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8000" });
+const API = axios.create({ baseURL: "https://sky-serve-geodata.vercel.app/" });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
